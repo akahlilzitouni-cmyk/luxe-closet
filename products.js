3f798f5261fd3a017952d955dba4525b78c2245e
luxe.closet.dz <akahlilzitouni@gmail.com>
2025-12-13 16:36:36 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,5 @@
+const products = [
+  {id:1, name:"تيشيرت عصري", color:"red", type:"casual", price:2500, desc:"راحة وأناقة", img:"./assets/shirt1.jpg"},
+  {id:2, name:"تيشيرت جديد", color:"blue", type:"sport", price:3000, desc:"تصميم شبابي", img:"./assets/shirt2.jpg"},
+  {id:3, name:"تيشيرت كلاسيكي", color:"green", type:"classic", price:2800, desc:"ألوان مميزة", img:"./assets/shirt3.jpg"}
+];
